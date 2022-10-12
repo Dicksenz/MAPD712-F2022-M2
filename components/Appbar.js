044c7342a4f3cb00c859265a59828ead6dbd4ef9
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-12 17:27:34 -0400

    custom appbar component added

---
+++ components/Appbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const Appbar = ({ title }) => {
+  return (
+    <View style={styles.appbarContainer}>
+      <Text style={styles.titleStyle}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  appbarContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    backgroundColor: "#347174",
+    height: 100,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+  },
+
+  titleStyle: {
+    color: "white",
+    fontSize: 20,
+    marginBottom: 10,
+  },
+});
+
+export default Appbar;
