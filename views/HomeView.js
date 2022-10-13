3d7a932723e8f92e3a995d64b529491cbcf426d3
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-12 20:39:57 -0400

    demo api added to home view to test flat list with pull to refresh

--- views/HomeView.js
+++ views/HomeView.js
@@ -4,21 +4,51 @@ import {
   View,
   ActivityIndicator,
   TouchableOpacity,
+  FlatList,
 } from "react-native";
 import CustomAppbar from "../components/CustomAppbar";
 import NoPatients from "../components/NoPatients";
 import Icon from "@expo/vector-icons/MaterialCommunityIcons";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import CustomLoader from "../components/CustomLoader";
 import FabButton from "../components/FabButton";
 
 const HomeView = ({ navigation }) => {
   const [isLoading, setisLoading] = useState(false);
   const [isEmpty, setIsEmpty] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getPatients = () => {
+    setisLoading(true);
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((response) => response.json())
+      .then((json) => {
+        setData(json);
+        setisLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setisLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    getPatients();
+  }, []);
 
   return (
-    <View>
-      {isLoading ? <CustomLoader /> : <NoPatients />}
+    <View style={{ height: "100%" }}>
+      {isLoading ? (
+        <CustomLoader />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => <Text>{item.title}</Text>}
+          onRefresh={() => getPatients()}
+          refreshing={isLoading}
+        />
+      )}
 
       <TouchableOpacity onPress={() => navigation.navigate("Add Patients")}>
         <FabButton />
