58f11de9658905b9807bc9f9a6923900fc1bd5c9
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-10-28 15:31:59 -0400

    logout clear async storage added

--- components/LogoutButton.js
+++ components/LogoutButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { View, TouchableOpacity, Text } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const LogoutButton = ({ navigation }) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
+      onPress={() => {
+        AsyncStorage.clear();
+      }}
+    >
       <Text style={{ color: "white" }}>Logout</Text>
     </TouchableOpacity>
   );
