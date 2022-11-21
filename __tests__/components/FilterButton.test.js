02401a21f2f17efa0051cf3c138b8c5efb77063b
Dicksen Veloopillay <dicksenveloopillay@Dicksens-MacBook-Pro.local>
2022-11-21 16:57:40 -0500

    get data when offline added to save filter

--- __tests__/components/FilterButton.test.js
+++ __tests__/components/FilterButton.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import renderer from "react-test-renderer";
 import FilterButton from "../../components/FilterButton";
 
-test("Check if CustomButton component renders correctly", () => {
+test("Check if FilterButton component renders correctly", () => {
   const tree = renderer.create(<FilterButton />).toJSON();
   expect(tree).toMatchSnapshot();
 });
